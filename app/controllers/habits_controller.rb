acbe7d4cbbd64d6026395a19d225de27dd793e8a
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2024-09-10 23:10:44 +0200

    habit create

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -2,4 +2,23 @@ class HabitsController < ApplicationController
   def index
     @habits = Habit.all
   end
+
+  def new 
+    @habit = Habit.new
+  end
+
+  def create 
+    @habit = Habit.new(habit_params)
+    if (@habit.save)
+      redirect_to habits_path, notice: "Hooray!🥳 New habit was created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def habit_params 
+    params.require(:habit).permit(:name)
+  end
 end
