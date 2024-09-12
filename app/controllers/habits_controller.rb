8b60c860b72d319af125f830dd7763690bf6114e
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2024-09-13 00:16:41 +0200

    edit habit

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,18 +1,32 @@
 class HabitsController < ApplicationController
+  before_action :find_habit, only: %w[edit update]
   def index
     @habits = Habit.all
   end
 
-  def new 
+  def new
     @habit = Habit.new
   end
-
+  
   def create 
     @habit = Habit.new(habit_params)
     if (@habit.save)
-      redirect_to habits_path, notice: "Hooray!🥳 New habit was created"
+      redirect_to habits_path, notice: "Hooray!🥳 Your new habit was created!"
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit 
+    @habit = Habit.find(params[:id])
+  end
+
+  def update
+    @habit = Habit.find(params[:id])
+    if @habit.update(habit_params)
+      redirect_to @habit, notice: "Your habit was updated!" 
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -20,5 +34,9 @@ class HabitsController < ApplicationController
   
   def habit_params 
     params.require(:habit).permit(:name)
+  end
+
+  def find_habit 
+    @habit = Habit.find(params[:id])
   end
 end
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,5 @@
 class Habit < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id, message: "You already have a habit with this name." }
+
+  belongs_to :user
 end
