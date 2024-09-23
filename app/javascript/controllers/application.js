7eedc30a0032069bc33b174b25d13dc7778efbd8
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2024-09-23 12:05:29 +0200

    notification toast

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import Notification from '@stimulus-components/notification'
 
 const application = Application.start()
+application.register('notification', Notification)
 
-// Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
